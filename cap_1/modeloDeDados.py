94b8dc8c5d690ba9401cad32c04d7073e31c089d
Gabriel <gabrielramosds@outlook.com>
2021-02-18 19:07:42 -0300

    First commit

---
+++ cap_1/modeloDeDados.py
@@ -0,0 +1,51 @@
+"""
+Baralho pythonico
+"""
+
+import collections
+from random import choice
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                       for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+beer_card = Card('7', 'diamods')
+print(beer_card)
+
+
+deck = FrenchDeck()  # Devido o dunder method ou método mágico __getitem__ minha classe fica iterável
+print(len(deck))
+print(choice(deck))
+"""
+for card in deck:
+    print(card)
+"""
+
+# Ordenação do baralho
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+# Iterando sobre baralho em ordem crescente
+"""
+for card in sorted(deck, key=spades_high):
+    print(card)
+"""
+
